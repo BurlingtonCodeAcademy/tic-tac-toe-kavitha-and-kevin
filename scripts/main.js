32b0a33639f50c87fc6c7075e543f150782b4c5c
kevinmahoney95 <kevinmahoney95@gmail.com>
2021-02-20 12:52:23 -0500

    CSS Updates

--- scripts/main.js
+++ scripts/main.js
@@ -42,16 +42,7 @@ function resetGame() {
   c8.style.backgroundColor = "";
 }
 
-/*const winningCombinations = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [2, 5, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [0, 4, 8],
-  [2, 4, 6]
-];*/
+
 
 function startNewGame() {
   boxes = ["", "", "", "", "", "", "", "", ""];
